Baron-von-Riedesel/VSBHDA

reorganized source paths part 2

Makefile
@@ -57,7 +57,7 @@ LINK=$(WATCOM)\binnt\wlink.exe
 FMTHX=
 !endif
 
-NAME=vsbhda
+NAME=VSBHDA
 
 !if $(DEBUG)
 OUTD=owd
@@ -121,21 +121,20 @@ all: $(OUTD) $(OUTD)\$(NAME).exe $(OUTD16)\$(NAME)16.exe
 $(OUTD):
 	@mkdir $(OUTD)
 
-$(OUTD)\$(NAME).exe: $(OUTD)\$(NAME).lib $(OUTD)\cstrtdhx.obj
+$(OUTD)\$(NAME).EXE: $(OUTD)\$(NAME).lib $(OUTD)\cstrtdhx.obj
 	@$(LINK) @<<
 format win pe $(FMTHX) runtime console
-file $(OUTD)\cstrtdhx, $(OUTD)\main, $(OUTD)\linear
-name $@
+file $(OUTD)\cstrtdhx, $(OUTD)\main, $(OUTD)\linear name $*.EXE
 libpath $(WATCOM)\lib386\dos;$(WATCOM)\lib386
 lib $(OUTD)\$(NAME).lib
 op q,m=$(OUTD)\$(NAME).map,stub=res\loadpero.bin,stack=0x10000,heap=0x1000
 $(CONSTATTR) $(CONST2ATTR)
 <<
 !if !$(USEJWL)
-	@res\patchpe $*.exe
+	@res\patchpe $*.EXE
 !endif
 
-$(OUTD16)\$(NAME)16.exe: .always
+$(OUTD16)\$(NAME)16.EXE: .always
 	@wmake -h -f OW16.mak debug=$(DEBUG) watcom=$(WATCOM) use19=$(USE19) usejwl=$(USEJWL)
 
 $(OUTD)\$(NAME).lib: $(OBJFILES)
@@ -198,7 +197,7 @@ $(OUTD)/rmwrap.obj:    src\rmwrap.asm src\rmcode1.asm src\rmcode2.asm
 
 clean: .SYMBOLIC
 	@wmake -h -f OW16.mak debug=$(DEBUG) clean
-	@del $(OUTD)\$(NAME).exe
+	@del $(OUTD)\$(NAME).EXE
 	@del $(OUTD)\$(NAME).lib
 	@del $(OUTD)\*.obj
 	@del $(OUTD)\rmcode?.bin

OW16.mak
@@ -40,8 +40,8 @@ LINK=$(WATCOM)\binnt\wlink.exe
 LIB=$(WATCOM)\binnt\wlib.exe
 ASM=jwasm.exe
 
-NAME=vsbhda16
-NAME2=sndcard
+NAME=VSBHDA16
+NAME2=SNDCARD
 
 !if $(DEBUG)
 OUTD=ow16d
@@ -104,25 +104,25 @@ LIBS=
 {src\startup}.c{$(OUTD)}.obj
 	@$(CC) $(C_DEBUG_FLAGS) $(C_OPT_FLAGS) $(C_EXTRA_FLAGS) $(CFLAGS) $(INCLUDES) -fo=$@ $<
 
-all: $(OUTD) $(OUTD)\$(NAME).exe $(OUTD)\$(NAME2).drv
+all: $(OUTD) $(OUTD)\$(NAME).EXE $(OUTD)\$(NAME2).DRV
 
 $(OUTD):
 	@mkdir $(OUTD)
 
-$(OUTD)\$(NAME).exe: $(OUTD)\$(NAME).lib $(OUTD)\cstrt16x.obj $(OUTD)\init1632.obj
+$(OUTD)\$(NAME).EXE: $(OUTD)\$(NAME).lib $(OUTD)\cstrt16x.obj $(OUTD)\init1632.obj
 	@$(LINK) @<<
 format dos 
-file $(OUTD)\init1632.obj, $(OUTD)\cstrt16x.obj, $(OUTD)\main.obj name $@
+file $(OUTD)\init1632.obj, $(OUTD)\cstrt16x.obj, $(OUTD)\main.obj name $*.EXE
 libpath $(WATCOM)\lib386\dos;$(WATCOM)\lib386
 lib $*.lib
 op q,statics,m=$*.map
 disable 80
 <<
 
-$(OUTD)\$(NAME2).drv: $(OUTD)\$(NAME2).lib $(OUTD)\dstrt16x.obj $(OUTD)\auexp16.obj
+$(OUTD)\$(NAME2).DRV: $(OUTD)\$(NAME2).lib $(OUTD)\dstrt16x.obj $(OUTD)\auexp16.obj
 	@$(LINK) @<<
 format dos 
-file $(OUTD)\dstrt16x.obj,$(OUTD)\auexp16.obj name $@
+file $(OUTD)\dstrt16x.obj,$(OUTD)\auexp16.obj name $*.DRV
 libpath $(WATCOM)\lib386\dos;$(WATCOM)\lib386
 lib $*.lib
 op q,statics,m=$*.map
@@ -201,8 +201,8 @@ $(OUTD)/rmwrap.obj:    src\rmwrap.asm src\rmcode1.asm src\rmcode2.asm
 clean: .SYMBOLIC
 	@del $(OUTD)\$(NAME).lib
 	@del $(OUTD)\$(NAME2).lib
-	@del $(OUTD)\$(NAME).exe
-	@del $(OUTD)\$(NAME2).drv
+	@del $(OUTD)\$(NAME).EXE
+	@del $(OUTD)\$(NAME2).DRV
 	@del $(OUTD)\*.obj
 	@del $(OUTD)\*.map
 	@del $(OUTD)\*.lst

drivelling-spinel/VSBCMI

Makefile
@@ -56,7 +56,7 @@ LINK=$(WATCOM)\binw\wlink.exe
 FMTHX=
 !endif
 
-NAME=vsbcmi
+NAME=VSBCMI
 
 !if $(DEBUG)
 OUTD=owd
@@ -122,21 +122,20 @@ all: $(OUTD) $(OUTD)\$(NAME).exe $(OUTD16)\$(NAME)16.exe $(OUTD)\UNINST.EXE
 $(OUTD):
 	@mkdir $(OUTD)
 
-$(OUTD)\$(NAME).exe: $(OUTD)\$(NAME).lib $(OUTD)\cstrtdhx.obj
+$(OUTD)\$(NAME).EXE: $(OUTD)\$(NAME).lib $(OUTD)\cstrtdhx.obj
 	*@$(LINK) @<<
 format win pe $(FMTHX) runtime console
-file $(OUTD)\cstrtdhx, $(OUTD)\main, $(OUTD)\linear
-name $@
+file $(OUTD)\cstrtdhx, $(OUTD)\main, $(OUTD)\linear name $*.EXE
 libpath $(WATCOM)\lib386\dos;$(WATCOM)\lib386
 lib $(OUTD)\$(NAME).lib
 op q,m=$(OUTD)\$(NAME).map,stub=res\loadpero.bin,stack=0x10000,heap=0x1000
 $(CONSTATTR) $(CONST2ATTR)
 <<
 !if !$(USEJWL)
-	@res\patchpe $*.exe
+	@res\patchpe $*.EXE
 !endif
 
-$(OUTD16)\$(NAME)16.exe: .always
+$(OUTD16)\$(NAME)16.EXE: .always
 	@wmake -h -f OW16.mak debug=$(DEBUG) watcom=$(WATCOM) use19=$(USE19) usejwl=$(USEJWL)
 
 $(OUTD)\$(NAME).lib: $(OBJFILES)
@@ -205,7 +204,7 @@ $(OUTD)\UNINST.EXE:    uninst\uninst.asm
 
 clean: .SYMBOLIC
 	@wmake -h -f OW16.mak debug=$(DEBUG) clean
-	@del $(OUTD)\$(NAME).exe
+	@del $(OUTD)\$(NAME).EXE
 	@del $(OUTD)\$(NAME).lib
 	@del $(OUTD)\*.obj
 	@del $(OUTD)\rmcode?.bin

OW16.mak
@@ -40,8 +40,8 @@ LINK=$(WATCOM)\binw\wlink.exe
 LIB=$(WATCOM)\binw\wlib
 ASM=jwasm.exe
 
-NAME=vsbcmi16
-NAME2=sndcard
+NAME=VSBCMI16
+NAME2=SNDCARD
 
 !if $(DEBUG)
 OUTD=ow16d
@@ -105,25 +105,25 @@ LIBS=
 {src\startup}.c{$(OUTD)}.obj
 	*@$(CC) $(C_DEBUG_FLAGS) $(C_OPT_FLAGS) $(C_EXTRA_FLAGS) $(CFLAGS) $(INCLUDES) -fo=$@ $<
 
-all: $(OUTD) $(OUTD)\$(NAME).exe $(OUTD)\$(NAME2).drv
+all: $(OUTD) $(OUTD)\$(NAME).EXE $(OUTD)\$(NAME2).DRV
 
 $(OUTD):
 	@mkdir $(OUTD)
 
-$(OUTD)\$(NAME).exe: $(OUTD)\$(NAME).lib $(OUTD)\cstrt16x.obj $(OUTD)\init1632.obj
+$(OUTD)\$(NAME).EXE: $(OUTD)\$(NAME).lib $(OUTD)\cstrt16x.obj $(OUTD)\init1632.obj
 	*@$(LINK) @<<
 format dos 
-file $(OUTD)\init1632.obj, $(OUTD)\cstrt16x.obj, $(OUTD)\main.obj name $@
+file $(OUTD)\init1632.obj, $(OUTD)\cstrt16x.obj, $(OUTD)\main.obj name $*.EXE
 libpath $(WATCOM)\lib386\dos;$(WATCOM)\lib386
 lib $*.lib
 op q,statics,m=$*.map
 disable 80
 <<
 
-$(OUTD)\$(NAME2).drv: $(OUTD)\$(NAME2).lib $(OUTD)\dstrt16x.obj $(OUTD)\auexp16.obj
+$(OUTD)\$(NAME2).DRV: $(OUTD)\$(NAME2).lib $(OUTD)\dstrt16x.obj $(OUTD)\auexp16.obj
 	*@$(LINK) @<<
 format dos 
-file $(OUTD)\dstrt16x.obj,$(OUTD)\auexp16.obj name $@
+file $(OUTD)\dstrt16x.obj,$(OUTD)\auexp16.obj name $*.DRV
 libpath $(WATCOM)\lib386\dos;$(WATCOM)\lib386
 lib $*.lib
 op q,statics,m=$*.map
@@ -205,8 +205,8 @@ $(OUTD)/rmwrap.obj:    src\rmwrap.asm src\rmcode1.asm src\rmcode2.asm
 clean: .SYMBOLIC
 	@del $(OUTD)\$(NAME).lib
 	@del $(OUTD)\$(NAME2).lib
-	@del $(OUTD)\$(NAME).exe
-	@del $(OUTD)\$(NAME2).drv
+	@del $(OUTD)\$(NAME).EXE
+	@del $(OUTD)\$(NAME2).DRV
 	@del $(OUTD)\*.obj
 	@del $(OUTD)\*.map
 	@del $(OUTD)\*.lst